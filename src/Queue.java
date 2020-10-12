b38bfbb5446cbadc55c5eab58b8840e38dc44b3d
unknown <dcalbarracinv@unal.edu.co>
2020-10-12 11:09:58 -0500

    Add queue implementation

---
+++ src/Queue.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+
+public class Queue<T> implements Iterable<T>{
+
+    private DoubleLinkedList<T> list = new DoubleLinkedList<T>();
+
+    public Queue(){}
+
+    public int size(){
+        return list.size();
+    }
+
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public T showFirst(){
+        if(isEmpty()){
+            throw new RuntimeException("Empty Queue");
+        }
+        return list.showFirst();
+    }
+
+    public T removeFirst(){
+        if(isEmpty()){
+            throw new RuntimeException("Empty Queue");
+        }
+        return list.removeFirst();
+    }
+
+    public void addBack(T element){
+        list.addLast(element);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+}
